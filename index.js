ef7cc5ffaa05c667a96d279b37355b14b959ca6b
wansong <betterofsong@gmail.com>
2017-02-22 21:11:19 +0800

    1.0.5: use module.exports

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import FlexiblePageListView from './FlexiblePageListView';
 import OctopusView from './OctopusView';
 import ScrollableTabBar from './ScrollableTabBar';
 
-export default {
+module.exports = {
   ListView,
   PagedListView,
   FlexiblePageListView,
